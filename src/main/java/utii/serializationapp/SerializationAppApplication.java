2ae8ae715a5e5feb3369e57a0eef0bed0df25858
sarvarsaidazov <mrsarvar418@gmail.com>
2025-01-30 20:46:51 +0500

    innit project

---
+++ src/main/java/utii/serializationapp/SerializationAppApplication.java
@@ -0,0 +1,13 @@
+package utii.serializationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerializationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerializationAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utii/serializationapp/SerializationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package utii.serializationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerializationAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
